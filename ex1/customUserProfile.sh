9209c29a873399c08c8eec8884016773b9eb413c
yehonatan <yona014@gmail.com>
2023-01-19 00:07:40 +0200

    bash ex1- try 18

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -10,6 +10,5 @@ if [[ -e "$TOKEN" ]]; then
     if [[ "$permissions" != "600" ]]; then
         echo "Warning: .token file has too wide permissions"
     fi
-export COURSE_ID="devsecops12"
 fi
-
+export COURSE_ID="devsecops12"
