a5077935b14f426ce4e3e2713e69241b871068d2
eli <eli4146@gmail.com>
2023-01-18 22:12:11 +0200

    exr1 a fix 2

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,18 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "Hello $USER"
-
-echo ""
 
 
 /usr/lib/update-notifier/apt-check --human-readable
 
 
-echo  "" 
 
-
-if [ -a ~/.token ] ; then  
- if [ "$(stat -c "%a" ~/.token)" ] != 600 ; then
+if [ -f ~/.token ] ; then
+ if [ "$(stat -c %a ~/.token)" != "600" ]   ; then
    echo "Warning: .token file has too wide permissions" 
  fi
 fi
