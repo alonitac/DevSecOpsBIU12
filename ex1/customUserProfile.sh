4743e8e622f8069c4a5b509f0ca1d76079a6aa36
yehonatan <yona014@gmail.com>
2023-01-18 23:12:14 +0200

    bash ex1- try 9

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -5,11 +5,11 @@ echo "Update available: $(/usr/lib/update-notifier/apt-check --human-readable)"
 echo "To see these additional updates run: apt list --upgrade"
 
 token_p=/home/$USER/.token
-if [ -e token_p ]; then
-    permissions=$(stat -c "%a" token_p)
-    if [ $permissions != "600" ]; then
+if [[ -e "$token_p" ]]; then
+    permissions=$(stat -c "%a" "$token_p")
+    if [[ $permissions != "600" ]]; then
         echo "Warning: .token file has too wide permissions"
     fi
+COURSE_ID=devsecops12
 fi
 
-COURSE_ID=devsecops12
