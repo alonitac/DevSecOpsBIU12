5bf16df7c9063998f916c43443d7445e9a586c27
hohifrog <hohironyanai@gmail.com>
2023-01-18 19:22:30 +0200

    First try

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,14 @@
-# your solution here...
+#!/bin/bash
+#Greetings to all user
+echo "Hello" $USER
+
+/usr/lib/update-notifier/apt-check --human-readable
+
+TOKEN_PATH=/home/$USER/.token
+if [[ -f "$TOKEN_PATH" ]]; then
+  PERM=$(stat -c "%a" "$TOKEN_PATH")
+  if [[ $PERM != "600" ]]; then
+    echo "Warning: .token file has too open permissions"
+  fi
+COURSE_ID="devsecops12"
+fi
