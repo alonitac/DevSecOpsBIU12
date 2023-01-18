2eedd9558b83b2c774d7874ea93ad4fb500a1df5
danielshahnovich <danielshvch@gmail.com>
2023-01-18 23:04:15 +0200

    Tested and working.

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,16 @@
 # your solution here...
+echo Hello $USER
+/usr/lib/update-notifier/apt-check --human-readable
+if [ -a ~/.token ]; then
+if [ $(stat -c %a ~/.token) != 600 ]; then
+echo "Warning: .token file has too wide permissions"
+fi
+else
+touch ~/.token
+chmod 600 ~/.token;
+echo ".token file created with permissions 600"
+fi
+export COURSE_ID=devsecops12
+chsh -s /bin/zsh #makes zsh default shell in the next logon
+zsh #switched immediately to zsh shell
+
