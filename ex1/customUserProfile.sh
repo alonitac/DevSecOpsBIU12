bfda6b6e768fc9ddb2b7746fafd5449ecb4f0726
yehonatan <yona014@gmail.com>
2023-01-18 22:04:45 +0200

    bash ex1 - final solution!

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,18 @@
-# your solution here...
+echo "Hello $USER"
+
+echo "Update available: $(/usr/lib/update-notifier/apt-check --human-readable)"
+echo "To see these additional updates run: apt list --upgrade"
+
+
+if [ -e ~/.token ]; then
+    permissions=$(stat -c "%a" ~/.token)
+    echo "Permissions for .token file: $permissions"
+    if [ $permissions != "600" ]; then
+        echo "Warning: .token file has too wide permissions"
+    fi
+else
+    echo ".token file does not exist in the home directory"
+fi
+
+
+export COURSE_ID=devsecops12
