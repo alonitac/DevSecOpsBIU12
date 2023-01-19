57b9e31744a465732a1b401e65fb60ef0042669b
eliraz <ovedeliraz96@gmail.com>
2023-01-19 10:26:15 -0800

    bash ex1 test1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,16 @@
-# your solution here...
+#!/bin/bash
+# Greetings
+echo "Hello $USER"
+
+# Print how many packages are outdated
+/usr/lib/update-notifier/apt-check --human-readable
+
+# Check the permissions of .token
+if [ -f ~/.token ]; then
+if [ $perms != "600" ]; then
+        echo "Warning: .token file has too wide permissions"
+    fi
+fi
+
+# Course variable
+export COURSE_ID="devsecops12"
