b32e0491efc3ac010dfc75af1a4b02a2b1bb6dd0
Victor L <fd7187771@gmail.com>
2023-01-19 12:25:33 +0200

    Fix 2

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -4,21 +4,22 @@
 #at start
 function filePermissionWarningMsg(){
   TEMP_FILE=$1
+  if [  -f $TEMP_FILE ];then
   FILE_PERM=$(stat -c "%a" "$TEMP_FILE")
 
-  if [[ $FILE_PERM != "600" ]]; then echo "Warning: $TEMP_FILE file has too open permissions"
+    if [[ $FILE_PERM != "600" ]]; then echo "Warning: $TEMP_FILE file has too open permissions"
+    fi
   fi
-
 }
 
 function mainExercise(){
 echo "Hello "$USER
 #? Next one to run and check
 /usr/lib/update-notifier/apt-check --human-readable
-TEMP_FILE=.token
-if [ ! -f $TEMP_FILE ];then
- touch $TEMP_FILE
-fi
+TEMP_FILE=/home/$USER/.token
+#if [ ! -f $TEMP_FILE ];then
+# touch $TEMP_FILE
+#fi
 filePermissionWarningMsg $TEMP_FILE
 export COURSE_ID="devsecops12"
 #echo $COURSE_ID
