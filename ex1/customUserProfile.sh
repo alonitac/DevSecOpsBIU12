2fddeb29e4cb5398dadf941365ed0c75ea56dacc
alonit <alon.itach@mail.huji.ac.il>
2023-01-19 10:10:44 +0200

    revert student mistake

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-#Print all user message
-echo "Hello" $USER
-#Print packages outdated
-/usr/lib/update-notifier/apt-check --human-readable
-#File permissions
-TOKEN_PATH=/home/$USER/.token
-if [[ -f "$TOKEN_PATH" ]]; then PERM=$(stat -c "%a" "$TOKEN_PATH")
-  if [[ $PERM != "600" ]]; then echo "Warning: .token file has too open permissions"
-  fi
-#Add an environment
-COURSE_ID="devsecops12"
-fi
-#Define zsh as the default shell for the user
-exec zsh
-
+# your solution here...
