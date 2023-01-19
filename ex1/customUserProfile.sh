c1bf93a6b5ef70f4997b2fd30242a5fdb5ad0e64
Guy-BP <guy66bp@gmail.com>
2023-01-19 16:45:59 +0200

    Bash ex1 - guy ben porath

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,11 +1,11 @@
-
-#!/bin/bash
+!/bin/bash
 
 echo "Hello $USER"
 /usr/lib/update-notifier/apt-check --human-readable
-if  [[ -e $HOME/.token ]]; then
-    if [ "stat -c '%a' .token" != "600" ]; then
+touch $HOME/.token
+if  [[ ! -e $HOME/.token ]]; then
+     touch $HOME/.token
+fi
+if [ "stat -c '%a' .token" != "600" ]; then
     echo "Warning: file has too open permissions"
-    fi
-
 fi
