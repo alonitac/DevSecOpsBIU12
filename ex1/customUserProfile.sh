a9a8f7fd534c0c6fc3d8c9142bed1fca6e8649c9
Zahi Toozon <toozonzahi@gmail.com>
2023-01-19 12:19:42 -0800

    Zahi Toozon

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,20 +1,28 @@
 # your solution here...
 
 #!/bin/bash
-export COURSE_ID=devsecops12
-chsh $(which zsh)
 
+#Greet the user
 echo Hello $USER
 echo
 
+#Print how many packages are outdated
 echo How many packages are outdated?
 /usr/lib/update-notifier/apt-check --human-readable
 echo
 
-echo >> $HOME/.token > test
+#Create .token file for
+echo >> /home/$USER/.token > test
 chmod 660 $HOME/.token
 
 if [ "stat -c "%a" $HOME/.token" != '600' ]; then
         echo "Warning: .token file has too open permissions"
         exit 1;
 fi
+
+#Set environment variable
+export COURSE_ID=devsecops12
+
+#Define zsh as the default shell
+chsh $(which zsh)
+
