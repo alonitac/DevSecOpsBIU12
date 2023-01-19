b4fa32a9f653e965ac8e3fe87ed131422c134666
YehonatanLevTovCYBER <yona014@gmail.com>
2023-01-19 11:52:57 +0200

    ex1 - F

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -4,13 +4,12 @@ echo "Hello $USER"
 echo "Update available: $(/usr/lib/update-notifier/apt-check --human-readable)"
 echo "To see these additional updates run: apt list --upgrade"
 
-TOKEN=/home/$USER/.token
-if [[ -e "$TOKEN" ]]; then
-    permissions=$(stat -c "%a" "$TOKEN")
+if [[ -e "/home/$USER/.token" ]]; then
+    permissions=$(stat -c "%a" "/home/$USER/.token")
     if [[ "$permissions" != "600" ]]; then
         echo "Warning: .token file has too open permissions"
     fi
-export COURSE_ID="devsecops12"
+COURSE_ID="devsecops12"
 fi
 
 
