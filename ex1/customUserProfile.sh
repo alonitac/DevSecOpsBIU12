72fcd8ae3132efe8ea582f7b0dc77fc5304ecb6d
Guy-BP <guy66bp@gmail.com>
2023-01-19 17:43:26 +0200

    Bash ex1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -6,7 +6,6 @@ if [ -e $HOME/.token]; then
     if [ "stat -c '%a' $HOME/.token" != "600" ]; then
    echo "Warning: .token file has too open permissions"
     fi
-else
 fi
 
 export COURSE_ID="devsecops12"
