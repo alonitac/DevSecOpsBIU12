6f0a91a33461a8e2cef2306e30b8952d63995b8c
ofek.harapz <oharpaz1@gmail.com>
2023-01-19 22:46:09 +0200

    Test1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -2,8 +2,20 @@
 
 # Variables Mpa
 COURSE_ID=devsecops12
+TOKEN_PARM=$(stat -c "%a" ~/.token)
+TOKEN_PATH=~/.token
+# Main Script
 
-# Script
-echo Hellow $USER
-echo You are at course $COURSE_ID
-echo $('/usr/lib/update-notifier/apt-check') --human-readable
+#Section1
+echo Hello $USER
+echo $COURSE_ID
+echo
+/usr/lib/update-notifier/apt-check --human-readable
+echo
+if [[ -e $TOKEN_PATH ]]; then [[ $TOKEN_PARM ]]
+    if [[ "$TOKEN_PARM" != "600"  ]]; then
+      echo "Warning: .token file has too wide permissions"
+      fi
+fi
+echo .token file perrmision is: $TOKEN_PARM
+echo Done!
