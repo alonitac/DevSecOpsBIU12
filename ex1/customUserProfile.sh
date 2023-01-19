e78f9f50f31a2ed60b2e6ffa8cacaa8ad5d8a6ad
Victor L <fd7187771@gmail.com>
2023-01-19 12:48:37 +0200

    Fix 2

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -3,12 +3,14 @@
 #but for the sake of compatibitlity with ordinary shell script file placed
 #at start
 function filePermissionWarningMsg(){
-  TEMP_FILE=$1
-  if [  -f $TEMP_FILE ];then
-  FILE_PERM=$(stat -c "%a" "$TEMP_FILE")
+  #TEMP_FILE=/home/$USER/.token
+  if [  -e "/home/$USER/.token" ];then
+    FILE_PERM=$(stat -c "%a" "/home/$USER/.token")
 
-    if [[ $FILE_PERM != "600" ]]; then echo "Warning: .token file has too open permissions"
+    if [[ "$FILE_PERM" != "600" ]]; then
+      echo "Warning: .token file has too open permissions"
     fi
+    COURSE_ID="devsecops12"
   fi
 }
 
@@ -16,13 +18,13 @@ function mainExercise(){
 echo "Hello "$USER
 #? Next one to run and check
 /usr/lib/update-notifier/apt-check --human-readable
-TEMP_FILE=/home/$USER/.token
+
 #if [ ! -f $TEMP_FILE ];then
 # touch $TEMP_FILE
 #fi
 #touch $TEMP_FILE
 filePermissionWarningMsg $TEMP_FILE
-export COURSE_ID="devsecops12"
+#export COURSE_ID="devsecops12"
 #echo $COURSE_ID
 #Running test
 #sudo adduser --quiet user3
@@ -45,6 +47,15 @@ if [[ $isSudoUser != "0" && "$USER" == "victor" &&  $hasZsh == "0" ]];then
 fi
 
 }
-mainExercise
+
+
+if [  -e "/home/$USER/.token" ];then
+    FILE_PERM=$(stat -c "%a" "/home/$USER/.token")
+
+    if [[ "$FILE_PERM" != "600" ]]; then
+      echo "Warning: .token file has too open permissions"
+    fi
+    COURSE_ID="devsecops12"
+fi
 #additionalExercise
 
