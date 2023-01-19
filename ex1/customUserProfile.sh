915ea9c462fc48c1d776d378832652ee1f5a5c4c
liorta <lior.taub@icloud.com>
2023-01-19 21:35:38 +0200

    First test ALLA AKBAR

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,9 @@
-# your solution here...
+#! /bin/bash
+echo Hello $USER
+echo $(/usr/lib/update-notifier/apt-check --human-readable)
+
+if [ -f /home/$USER/.token ]; then
+  stat -c "%a" /home/$USER/.token ; then if [ $? != 600 ]; then echo "Warning: .token file has too open permissions
+"
+    
+fi
