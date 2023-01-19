0267f6a1113f2eeb91a1b579536fcb49d6a9f276
hohifrog <hohironyanai@gmail.com>
2023-01-19 06:24:46 +0200

    Add zsh as default

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#Greetings to all user
+#Print all user message
 echo "Hello" $USER
-
+#Print packages outdated
 /usr/lib/update-notifier/apt-check --human-readable
-
+#File permissions
 TOKEN_PATH=/home/$USER/.token
-if [[ -f "$TOKEN_PATH" ]]; then
-  PERM=$(stat -c "%a" "$TOKEN_PATH")
-  if [[ $PERM != "600" ]]; then
-    echo "Warning: .token file has too open permissions"
+if [[ -f "$TOKEN_PATH" ]]; then PERM=$(stat -c "%a" "$TOKEN_PATH")
+  if [[ $PERM != "600" ]]; then echo "Warning: .token file has too open permissions"
   fi
+#Add an environment
 COURSE_ID="devsecops12"
 fi
+#Define zsh as the default shell for the user
+exec zsh
