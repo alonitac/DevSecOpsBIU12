f26df75558a38c904c3fdcdd000b494163f20b0e
Zahi Toozon <toozonzahi@gmail.com>
2023-01-19 11:29:15 -0800

    Default profile for new Linux Users Created By Zahi Toozon

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,16 @@
 # your solution here...
+#!/bin/bash
+export COURSE_ID=devsecops12
+chsh -s $(which zsh)
+
+echo Hello $USER
+echo
+
+echo How many packages are outdated?
+/usr/lib/update-notifier/apt-check --human-readable
+echo
+
+if [ "stat -c "%a" $HOME/token.sh" != '600' ]; then
+        echo "Warning: .token file has too open permissions"
+        exit 1;
+fi
