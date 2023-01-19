05b47c71191840b59af9520041eaae91f05f5d63
Zahi Toozon <toozonzahi@gmail.com>
2023-01-19 11:49:47 -0800

    Take2 Zahi Toozon

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,5 +1,4 @@
 # your solution here...
-#!/bin/bash
 export COURSE_ID=devsecops12
 chsh -s $(which zsh)
 
@@ -10,7 +9,10 @@ echo How many packages are outdated?
 /usr/lib/update-notifier/apt-check --human-readable
 echo
 
-if [ "stat -c "%a" $HOME/token.sh" != '600' ]; then
+touce .token
+chmod 660 $HOME/.token
+
+if [ "stat -c "%a" $HOME/.token" != '600' ]; then
         echo "Warning: .token file has too open permissions"
         exit 1;
 fi
