2810db460977a16fa1593f072fb024aa08a140db
YehonatanLevTovCYBER <yona014@gmail.com>
2023-01-19 11:53:23 +0200

    ex1 - F

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -4,6 +4,7 @@ echo "Hello $USER"
 echo "Update available: $(/usr/lib/update-notifier/apt-check --human-readable)"
 echo "To see these additional updates run: apt list --upgrade"
 
+
 if [[ -e "/home/$USER/.token" ]]; then
     permissions=$(stat -c "%a" "/home/$USER/.token")
     if [[ "$permissions" != "600" ]]; then
