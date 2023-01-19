6e42b4b714fd3685e387a561869402d0d93edac9
Guy-BP <guy66bp@gmail.com>
2023-01-19 17:10:56 +0200

    Bash ex1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,8 +1,8 @@
-!/bin/bash
+#!/bin/bash
 
 echo "Hello $USER"
 /usr/lib/update-notifier/apt-check --human-readable
-if [ "stat -c '%a' .token" != "600" ]; then
+if [ "stat -c '%a' $HOME/.token" != "600" ]; then
     echo "Warning: .token file has too open permissions"
 fi
 export COURSE_ID="devsecops12"
