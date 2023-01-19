7566e78bdede19cbe2ae6a11fc205bdf1ac0e552
ofek.harapz <oharpaz1@gmail.com>
2023-01-19 18:41:02 +0200

    כדגכדג

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,9 @@
-# your solution here...
+#! /bin/bash
+
+# Variables Mpa
+COURSE_ID=devsecops12
+
+# Script
+echo Hellow $USER
+echo You are at course $COURSE_ID
+echo $('/usr/lib/update-notifier/apt-check') --human-readable
