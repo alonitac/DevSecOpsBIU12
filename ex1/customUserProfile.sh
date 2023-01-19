22b83eb7f8ca3f669a672756287609cd3d9c0314
eliraz <ovedeliraz96@gmail.com>
2023-01-19 11:14:10 -0800

    bash ex1 test2

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -6,11 +6,6 @@ echo "Hello $USER"
 # Print how many packages are outdated
 /usr/lib/update-notifier/apt-check --human-readable
 
-# Create .token file in the user's home directory
-if [ ! -f ~/.token ]; then
-    touch ~/.token
-    chmod 600 ~/.token
-fi
 
 # Check the permissions of .token
 if [ -f ~/.token ]; then
@@ -18,8 +13,7 @@ if [ -f ~/.token ]; then
     if [ $perms != "600" ]; then
         echo "Warning: .token file has too wide permissions"
     fi
-fi
 
 # Course variable
-export COURSE_ID="devsecops12"
+COURSE_ID="devsecops12"
 fi
