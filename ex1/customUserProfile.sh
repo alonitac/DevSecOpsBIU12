92b3d77aefc9554c6503d90efb8b76c796dd4461
Zahi Toozon <toozonzahi@gmail.com>
2023-01-19 11:52:41 -0800

    Zahi Toozon

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -9,7 +9,7 @@ echo How many packages are outdated?
 /usr/lib/update-notifier/apt-check --human-readable
 echo
 
-touce .token
+touce $HOME/.token
 chmod 660 $HOME/.token
 
 if [ "stat -c "%a" $HOME/.token" != '600' ]; then
