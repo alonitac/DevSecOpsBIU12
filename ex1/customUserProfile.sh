c33fa7962163e2d474019ca68e97637fd5843f1b
OmerAvrech <omeravrech@gmail.com>
2023-01-20 11:56:54 +0200

    Submitting exercise 1 - Hello user - Package notification - Verify token privileges - export COURSE_ID variable - define ZSH as terminal

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,36 @@
-# your solution here...
+#!/bin/bash
+
+# CONST
+TOKEN="/home/$USER/.token"
+TERMINAL="$(which zsh)"
+
+
+
+
+# WELCOME NOTIFICATION
+echo "Hello $USER"
+echo
+
+# DISPLAY AVALIABLE UPDATES
+/usr/lib/update-notifier/apt-check --human-readable
+echo
+
+# CHECK IF THE TOKEN IS EXIST
+if [[ -e $TOKEN ]]; then
+    # File exist, continue with checks
+    if [ $(stat -c "%a" "$TOKEN") -ne 600 ]; then
+        # File is existed and protected
+        echo "Warning: $TOKEN file has too open permissions"
+    fi
+fi
+echo
+
+
+# DEFINE ENVIRONMENT VARIABLES
+export COURSE_ID="devsecops12"
+
+
+# DEFINE ZSH TERMINAL
+if [[ -e "$TERMINAL" ]]; then
+    $TERMINAL -0
+# END OF SCRIPT
