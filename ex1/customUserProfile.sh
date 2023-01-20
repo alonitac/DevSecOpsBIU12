85cdf542a2029e58e583592bd84c07f2f1c9ac96
OmriYahav <omriyx@gmail.com>
2023-01-20 13:17:52 +0200

    Fix with comments

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -2,13 +2,24 @@
 #Greetings to all user
 echo "Hello" $USER
 
-/usr/lib/update-notifier/apt-check --human-readable
+#Print how many packages are outdated
+PackagesOutdated= /usr/lib/update-notifier/apt-check --human-readable
+echo $PackagesOutdated
 
-TOKEN_PATH=/home/$USER/.token
-if [[ -f "$TOKEN_PATH" ]]; then
-  PERM=$(stat -c "%a" "$TOKEN_PATH")
-  if [[ $PERM != "600" ]]; then
+#check the file permissions,If the octal representation of the permissions set is different from 600
+TokenPath=/home/$USER/.token
+if [[ -f "$TokenPath" ]];
+ then
+  UserPerm=$(stat -c "%a" "$TokenPath")
+  if [[ $UserPerm != "600" ]]; then
+    #print a warning message to the user
     echo "Warning: .token file has too open permissions"
   fi
+  #environment variable called COURSE_ID with a value equals to devsecops12
 COURSE_ID="devsecops12"
+
+#change the default shell to zsh for the newuser
+chsh -s /usr/bin/zsh
+
 fi
+
