46e5ad9bcbb968a7e4504a77753640b864f42a78
roman <romagaon@gmail.com>
2023-01-20 07:09:52 -0800

    test3

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Greet the user
-
-echo "Hello ${USER}"
-
-# Print how many packages are otdate
-/usr/lib/update-notifier/apt-check --human-readable
-
-# Check the file permission
-TokenPATH=/home/$USER/.token
-if [[ -f "$TokenPATH" ]]; then UserPerm=$(stat -c "%a" "$TokenPATH")
-if [[ $UserPerm != "600" ]]; then print "Warning: .token file has too open permissions"
-
-# Add Course_ID
-COURSE_ID="devsecops12"
-fi
+	# Greet the user
+	echo "Hello $USER"
+	# Print how many packages are outdated
+	/usr/lib/update-notifier/apt-check --human-readable
+	#  Checking the file permission
+	TOKEN_PATH=/home/$USER/.token
+	if [[ -f "$TOKEN_PATH" ]]; then USERPERM=$(stat -c "%a" "$TOKEN_PATH")
+	  if [[ $USERPERM != "600" ]]; then echo "Warning: .token file has too open permissions"
+	  fi
+	# Course ID
+	COURSE_ID="devsecops12"
+	fi
+	# End EX1
