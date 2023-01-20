99002f26678698c5b3bbb7d34a8e3d19fd2ddc6f
aviv singer <aviv.zinger@gmail.com>
2023-01-20 00:18:55 -0800

    solution to ex1 try 3

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -6,14 +6,14 @@ echo "Hello $USER"
 /usr/lib/update-notifier/apt-check --human-readable 
 
 #now we check if there is .token file at home directory if yes will check permissions 
-if [ -f ~/token ] ;
+if [ -f ~/.token ]
 then
-if [ "(stat -c %a ~/.token)" != "600"] ;
+if [ "(stat -c "%a" ~/.token)" != "600" ]
 then 
 echo "Warning: .token file has too wide permissions"
 fi
 
 #environment variable called COURSE_ID with a value equals to devsecops12:
-COURSE_ID="devsecops12";
+export COURSE_ID="devsecops12"
 fi
 
