04fa9adc729288bf42bf36d76306e376b33ab6cc
amit <amitmainemer@gmail.com>
2023-01-20 10:56:11 +0200

    wip: try3

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -10,13 +10,10 @@ echo Hello "$USER"
 /usr/lib/update-notifier/apt-check --human-readable
 
 if [ -f $TOKEN_FILE ]; then
-PERM=$(stat -c %a $TOKEN_FILE)
-if [ $PERM != $SAFE_PERMISSIONS ]; then
-    echo $WARNING_MESSAGE
-fi
-else
-  touch $TOKEN_FILE
-  chmod $SAFE_PERMISSIONS $TOKEN_FILE
+  PERM=$(stat -c %a $TOKEN_FILE)
+  if [[ $PERM != $SAFE_PERMISSIONS ]]; then
+      echo $WARNING_MESSAGE
+  fi
 fi
 export COURSE_ID=devsecops12
 chsh -s $DEFAULT_SHELL
