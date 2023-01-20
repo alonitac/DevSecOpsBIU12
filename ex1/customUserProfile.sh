454500a251a04e48086e59adb63cc6120fedf386
tomy1230 <tomy1230@gmail.com>
2023-01-20 00:48:53 -0800

    fixNew1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,33 @@
 # your solution here...
+#!/bin/bash
+# greet the user
+
+echo "Hello ${USER}"
+
+UPDATE= /usr/lib/update-notifier/apt-check --human-readable
+
+ # display the count of updates
+
+echo  $UPDATE
+
+
+# create .token file
+
+touch $HOME/.token
+# check file permissions
+
+TOKEN=$(stat -c "%a" $HOME/.token)
+NUM='600'
+
+
+
+# compare permissions
+if [ -a $HOME/.token ] ; then
+if [ $TOKEN != $NUM ] ; then
+
+echo " Warning: .token file has too wide permissions "
+fi
+fi
+
+
+COURSE_ID=devsecops12
