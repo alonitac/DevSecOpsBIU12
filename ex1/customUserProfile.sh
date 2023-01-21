1f2c58833cffb0b11f8f6a65ec47e2ab1eef2bb8
ofek.harpaz <oharpaz1@Gmail.com>
2023-01-21 05:38:41 -0800

    test4 too open alek

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -2,8 +2,9 @@
 
 # Variables Mpa
 COURSE_ID=devsecops12
-TOKEN_PARM=$(stat -c "%a" /home/$USER/.token)
-TOKEN_PATH=/home/$USER/.token
+TOKEN_PARM=$(stat -c "%a" ~/.token)
+TOKEN_PATH=~/.token
+USER=MotherFUcker
 
 # Greetings
 echo "Hello $USER"
@@ -12,12 +13,14 @@ echo "Hello $USER"
 
 # Check the permissions of .token
 if [[ -f $TOKEN_PATH ]]; then [[ $TOKEN_PARM ]]
-    if [[ $TOKEN_PARM != "600" ]]; then
-      echo "Warning: .token file has too wide permissions"
+    if [[ $TOKEN_PARM != 600 ]]; then
+      echo "Warning: .token file has too open permissions"
     fi
-    echo .token file perrmision is: $TOKEN_PARM
+    echo .token file perrmision is: "$TOKEN_PARM"
     echo Done check the permissions of .token
 # Course variable
     echo "$COURSE_ID"
     echo Script done!
 fi
+
+echo Done!!!!!!!!!
