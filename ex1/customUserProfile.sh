06b3a1b56f69ba677b64f33c9b40b7c5c88ba53f
ofek.harpaz <oharpaz1@Gmail.com>
2023-01-21 05:23:05 -0800

    test3

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,23 +1,23 @@
-#! /bin/bash
+#!/bin/bash
 
 # Variables Mpa
 COURSE_ID=devsecops12
-TOKEN_PARM=$(stat -c "%a" "$TOKEN_PATH")
-TOKEN_PATH=~/.token
-# Main Script
+TOKEN_PARM=$(stat -c "%a" /home/$USER/.token)
+TOKEN_PATH=/home/$USER/.token
 
 # Greetings
 echo "Hello $USER"
 # Print how many packages are outdated
 /usr/lib/update-notifier/apt-check --human-readable
+
 # Check the permissions of .token
-if [[ -f "$TOKEN_PATH" ]]; then "$TOKEN_PARM"
-    if [[ $TOKEN_PARM != "600"  ]]; then
+if [[ -f $TOKEN_PATH ]]; then [[ $TOKEN_PARM ]]
+    if [[ $TOKEN_PARM != "600" ]]; then
       echo "Warning: .token file has too wide permissions"
-      fi
-fi
-echo .token file perrmision is: "$TOKEN_PARM"
-echo Done!
+    fi
+    echo .token file perrmision is: $TOKEN_PARM
+    echo Done check the permissions of .token
 # Course variable
-echo "$COURSE_ID"
-echo Done!
+    echo "$COURSE_ID"
+    echo Script done!
+fi
