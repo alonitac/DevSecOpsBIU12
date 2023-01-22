f3ecb3b602d7787213e571f679ca8618d695b4b3
gal <galdevops@gmail.com>
2023-01-22 08:12:44 +0200

    ex1 by Gal_Y_02 - homework ex1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,13 @@
-# your solution here...
+echo "Hello $USER"
+# Print outdated
+/usr/lib/update-notifier/apt-check --human-readable
+# Check if token file exist and the permissions of .token
+TOKEN_PATH=/home/$USER/.token
+if [[ -f "$TOKEN_PATH" ]]; then PERM=$(stat -c "%a" "$TOKEN_PATH")
+   if [[ $PERM != "600" ]]; then echo "Warning: .token file has too open permissions"
+
+fi
+
+fi
+# Course variable
+COURSE_ID="devsecops12"
