5ffc91c111e62891daabff5ee40fc302caa6d183
aviv singer <aviv.zinger@gmail.com>
2023-01-22 09:49:24 -0800

    solution to ex1 try 3 fix

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,19 +1,23 @@
 #!bin/bash
 #Greeing the user:
 echo "Hello $USER"
+echo ""
+
+
 
 #Print how many packages are outdated:
 /usr/lib/update-notifier/apt-check --human-readable 
+echo ""
 
-#now we check if there is .token file at home directory if yes will check permissions 
-if [ -f ~/.token ]
-then
-if [ "(stat -c "%a" ~/.token)" != "600" ]
-then 
-echo "Warning: .token file has too open permissions"
+#now we check if there is .token file at home directory if yes will check permissions  
+if [ "$(stat -c "%a" ~/.token)" != "600" ]
+ then echo "Warning: .token file has too open permissions"
 fi
 
 #environment variable called COURSE_ID with a value equals to devsecops12:
 export COURSE_ID="devsecops12"
-fi
+echo ""
+
+
+
 
