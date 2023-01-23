e359d16754dbf31b668172cb1d2cde536795134c
ronperez <perez.ron@protonmail.com>
2023-01-23 17:57:37 +0200

    update token file check

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -11,14 +11,16 @@ echo -e "hello $USERNAME \n"
 echo -e "$($APTPATH/apt-check --human-readable) \n"
 
 # .token file permission check
-if [[ ! -f $USERHOME/$FILE ]]; then
-	exit
-else
+if [[ -f $USERHOME/$FILE ]]; then
 	if [ '$(stat -c "%a" $USERHOME/$FILE)' > 600 ]; then
 		echo "Warning: $FILE has too open permissions"
-		exit 0
 	fi
 fi
+
+#Set Environment variable COURSE_ID >> devsecops12
+COURSE_ID='devsecops12'
+
+#Set ZSH as default shell
 
 #exit program
 exit 0
