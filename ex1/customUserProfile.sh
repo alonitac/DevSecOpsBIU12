f18b7f7271393a08c7aab039a696d6d2db152dc5
ronperez <perez.ron@protonmail.com>
2023-01-23 17:48:20 +0200

    first attempt

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,24 @@
-# your solution here...
+#!/bin/bash
+
+APTPATH="/usr/lib/update-notifier"
+USERHOME=$HOME
+FILE=".token"
+
+#greetings
+echo -e "hello $USERNAME \n"
+
+#print
+echo -e "$($APTPATH/apt-check --human-readable) \n"
+
+# .token file permission check
+if [[ ! -f $USERHOME/$FILE ]]; then
+	exit
+else
+	if [ '$(stat -c "%a" $USERHOME/$FILE)' > 600 ]; then
+		echo "Warning: $FILE has too open permissions"
+		exit 0
+	fi
+fi
+
+#exit program
+exit 0
