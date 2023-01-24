c273bb8b2af318a88d83bfb35a26f7ea6dcc897f
yuval <yuvalarviv86@gmail.com>
2023-01-24 05:11:09 -0800

    result

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,17 @@
-# your solution here...
+# Greet the user
+echo "Hello $USER"
+
+# Check for outdated packages
+outdated=$(/usr/lib/update-notifier/apt-check --human-readable)
+echo "$outdated"
+
+# Check file permissions of .token file
+if [ -f /home/yuval/.token ]; then
+  permissions=$(stat -c "%a" ~/.token)
+  if [ "$permissions" != "600" ]; then
+    echo "Warning: .token file has too open permissions"
+  fi
+fi
+
+# Set COURSE_ID environment variable
+export COURSE_ID=devsecops12
