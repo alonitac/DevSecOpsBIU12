97eeb8dc3f09e013419ba1460c0aa5cf0a43a62e
danielshahnovich <danielshvch@gmail.com>
2023-01-24 17:30:22 +0200

    Tested and working.

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -5,10 +5,6 @@ if [ -a ~/.token ]; then
 if [ $(stat -c %a ~/.token) != 600 ]; then
 echo "Warning: .token file has too wide permissions"
 fi
-else
-touch ~/.token
-chmod 600 ~/.token;
-echo ".token file created with permissions 600"
 fi
 export COURSE_ID=devsecops12
 chsh -s /bin/zsh #makes zsh default shell in the next logon
