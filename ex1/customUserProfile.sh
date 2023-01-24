0944995cddd47843ceb9b0071cec870efb2990c2
Haifranco <122602441+Haifranco@users.noreply.github.com>
2023-01-24 11:17:54 -0800

    Update customUserProfile.sh

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,19 @@
-# your solution here...
+#!/bin/bash
+
+# Print user  welcome
+ echo $USER Hello
+
+#print number of out of date softwares
+/usr/lib/update-notifier/apt-check --human-readable | grep updates
+#create token file
+sudo touch /home/$USER.token
+
+TOKEN_PATH=/home/$USER.token
+
+ab=${stat -c "%a" TOEKN_PATH} 
+
+if [ab -g 600]
+then
+        echo "good news all fine"
+else
+        echo "warning security issues"
