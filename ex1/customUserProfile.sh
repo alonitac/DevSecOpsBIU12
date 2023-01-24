c748db2ea8733e66d09f3ed625098a99c03a7451
danielshahnovich <danielshvch@gmail.com>
2023-01-24 18:12:54 +0200

    Tested and working. without zsh

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -6,7 +6,7 @@ if [ $(stat -c %a ~/.token) != 600 ]; then
 echo "Warning: .token file has too open permissions"
 fi
 fi
-export COURSE_ID=devsecops12
+# export COURSE_ID=devsecops12
 # chsh -s /bin/zsh #makes zsh default shell in the next logon
 # zsh #switched immediately to zsh shell
 
