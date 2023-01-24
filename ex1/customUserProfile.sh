f91db401fa3e0069be9e0a6b7066198257c74e3e
Guy-BP <guy66bp@gmail.com>
2023-01-24 17:27:27 +0200

    Bash ex1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -8,6 +8,8 @@ if [[ -f /home/$USER/.token ]]; then
    if [[ "stat -c '%a' /home/$USER/.token" != "600" ]]; then
      echo "Warning: .token file has too open permissions"
    fi
+   else
+     exit
 fi
 COURSE_ID="devsecops12"
 exit
