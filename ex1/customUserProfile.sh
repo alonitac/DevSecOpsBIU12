8f3413054873c1a31ae4b88578de938b0e3b11f2
ido <idocarmi011@gmail.com>
2023-01-24 14:23:22 -0800

    v1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,15 @@
-# your solution here...
+# Greet the user
+echo "Hello $USER"
+
+# Check for outdated packages
+outdated=$(/usr/lib/update-notifier/apt-check --human-readable)
+echo "$outdated"
+
+# Check file permissions of .token file
+if [ -f ~/.token ]; then
+  permissions=$(stat -c "%a" ~/.token)
+  if [ "$permissions" != "600" ]; then
+    echo "Warning: .token file has too open permissions"
+
+# Set COURSE_ID environment variable
+# export COURSE_ID=devsecops12
