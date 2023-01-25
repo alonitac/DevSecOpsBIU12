c92e024584b0f1b35848915d934632519a897545
Eli Levy <101037880+ellv41@users.noreply.github.com>
2023-01-25 20:32:25 +0200

    test 4

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -4,7 +4,7 @@
     /usr/lib/update-notifier/apt-check --human-readable
     if  test -f /home/$USER/.token ; then
       echo $(stat -c "%a" "/home/$USER/.token")
-     if (($(stat -c "%a" "/home/$USER/.token") >= 600)) ; then
+     if (($(stat -c "%a" "/home/$USER/.token") > 600)) ; then
            echo 'Warning: .token file has too open permissions'
      fi
     fi
