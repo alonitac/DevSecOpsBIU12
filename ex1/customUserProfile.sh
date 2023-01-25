85e2a0f61a0be11b256386098ae3e7db25bd24b1
Sahar <sarjanna@gmail.com>
2023-01-25 16:01:14 +0200

    test

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -7,7 +7,7 @@
     if ! test -f /home/.token
     then
         exit
-    elif (($(stat -c %a "/home/.token") != 600))
+    elif (($(stat -c %a "/home/.token") != "600"))
     then
         echo 'Warning: .token file has too open permissions'
     else
