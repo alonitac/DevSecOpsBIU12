826ce3f72ed5671e31e6081257a6a485f695695b
Sahar <sarjanna@gmail.com>
2023-01-25 13:08:20 +0200

    test

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -8,7 +8,7 @@
     if ! test -f /home/.token
     then
       exit
-    elif ($(stat -c %a "/home/.token") != 600 ))
+    elif (($(stat -c %a "/home/.token") != 600 ))
     then
       echo Warning: .token file has too wide permissions
     else
