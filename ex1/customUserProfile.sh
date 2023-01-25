7b4c69108052e282c87b366875c0f3499c533c1d
Sahar <sarjanna@gmail.com>
2023-01-25 12:02:52 +0200

    test

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -4,14 +4,16 @@
     cd /usr/lib/update-notifier
     ./apt-check --human-readable
     echo '' 'update can be applied immediately.'
-    echo To see these additional updates run : apt-list --upgrade
+   # echo To see these additional updates run : apt-list --upgrade
     if ! test -f /home/.token
     then
       exit
     fi
-    per=$(stat -c %a /home/.token)
-    if [ "$per" != "$600" ];
+    per=$(stat -c %a '/home/.token')
+    if (($per != 600 ))
     then
        echo Warning: .token file has too wide permissions
+    ELSE
+       EXIT
     fi
     export COURSE_ID=devsecops12
