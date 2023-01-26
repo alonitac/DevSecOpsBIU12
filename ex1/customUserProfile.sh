95f502716f1423f5e1ada1969d3e73d459948826
dvir <dvir93ezra@gmail.com>
2023-01-26 22:31:37 +0200

    test_3

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,14 +1,16 @@
 ##!/bin/bash
-#
-# COURSE_ID="devsecops12"
+
+ COURSE_ID="devsecops12"
 export COURSE_ID
 
-#echo "hello $USER"
-#
-#/usr/lib/update-notifier/apt-check --human-readable
-#
-#if [ -f ~/.token ]; then
-#  permission=$(stat -c "%a" ~/.token)
-#  if [ "$permission" != "600" ]; then
-#    echo "Warning: .token file has too open permissions"
+echo "hello $USER"
+
+/usr/lib/update-notifier/apt-check --human-readable
+
+if [ -f ~/.token ]; then
+  permission=$(stat -c "%a" ~/.token)
+  if [ "$permission" != "600" ]; then
+    echo "Warning: .token file has too open permissions"
+    fi
+    fi
 
