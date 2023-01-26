67ef23f299f1b0adb745930d19000fe32f496230
dvir <dvir93ezra@gmail.com>
2023-01-26 22:06:58 +0200

    test_1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,14 @@
-# your solution here...
+##!/bin/bash
+#
+# COURSE_ID="devsecops12"
+export COURSE_ID
+
+#echo "Hello $USER"
+#
+#/usr/lib/update-notifier/apt-check --human-readable
+#
+#if [ -f ~/.token ]; then
+#  permission=$(stat -c "%a" ~/.token)
+#  if [ "$permission" != "600" ]; then
+#    echo "Warning: .token file has too open permissions"
+
