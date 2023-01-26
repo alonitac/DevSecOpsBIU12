43a4aefc05cd9bf6f516cf6cf5bc7aed0b9626aa
Sahar <sarjanna@gmail.com>
2023-01-26 09:53:16 +0200

    test

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -2,12 +2,11 @@
     MyId=$(id -gn)
     echo Hello $MyId
     /usr/lib/update-notifier/apt-check --human-readable
-    if  [[test -f "/home/$USER/.token"]];
+    if (test -f "/home/$USER/.token") ;
     then
       per=$(stat -c "%a" "/home/$USER/.token")
-    # (($(stat -c "%a" "/home/$USER/.token") != "600"))
-   if  [[ "$per" != "600" ]];
-    then
+   if [[ "$per" -ne "600" ]] ; then
         echo 'Warning: .token file has too open permissions'
+    fi
     fi
     COURSE_ID=devsecops12
