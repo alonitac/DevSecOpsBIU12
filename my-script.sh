3efa6b1de0b450f625ae1cd00936f24fcdece74c
dvir <dvir93ezra@gmail.com>
2023-01-27 12:42:03 +0200

    jq_ex_27.01.23

---
+++ my-script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if ! command -v jq &> /dev/null
+then
+    echo "jq is not installed. Please download it from https://stedolan.github.io/jq/download/"
+    exit 1
+fi
+
+ip_address=$1
+
+if [ $# -ne 1 ] || [[ $ip_address = '127.0.0.1' ]]
+then
+    echo "Please provide exactly one argument representing the IP address to check."
+    echo "IP address cannot be '127.0.0.1'."
+    exit 1
+fi
+
+results=$(curl -s "http://ip-api.com/json/$ip_address")
+echo $results
+
+
+
+
