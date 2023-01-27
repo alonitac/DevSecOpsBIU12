320f699a9b47d062d022158c8b67d3fa9345ce83
elirambe <eliram26@gmail.com>
2023-01-27 04:41:42 -0800

    test_1_elirambe_15

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -6,11 +6,9 @@ echo "Hello $USER"
 
 /usr/lib/update-notifier/apt-check --human-readable
 
-if [ -f ~/.token ]
-then
+if [ -f ~/.token ]; then
   permission=$(stat -c "%a" ~/.token)
-  if [ "$permission" != "600" ]
-  then
-    echo "Warning: .token file has too wide permissions"
+  if [ "$permission" != "600" ]; then
+    echo "Warning: .token file has too open permissions"
     fi
-  fi
+    fi
