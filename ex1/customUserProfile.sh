431cc18071abe7aedf4ff68694fdb55787d112b0
elirambe <eliram26@gmail.com>
2023-01-27 04:23:03 -0800

    test_1_elirambe_1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,16 @@
-# your solution here...
+#!/bin/bash
+
+echo "hello $USER"
+
+/usr/lib/update-notifier/apt-check --human-readable
+
+if [ -f ~/.token ]
+then
+  permission=$(stat -c "%a" ~/.token)
+
+  if [ "$permission" != "600" ]
+  then
+    echo "Warning: .token file has too wide permissions"
+    fi
+
+  fi
