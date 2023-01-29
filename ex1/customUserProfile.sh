4d4ced66b2504d9b134e6b13b66fad17cfe1bb51
ronperez <perez.ron@protonmail.com>
2023-01-29 17:56:19 +0200

    removed zsh code for testing

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -11,23 +11,20 @@ echo "Hello" $USER
 #print packages updates
 echo $CHECK_UPDATES
 
-
 # .token file permission check
 if [ -f $FILEPATH ] && [ $(stat -c "%a" $FILEPATH) != "600" ] ; then
-        echo  "Warning: $FILENAME file has too open permissions"
+  echo  "Warning: $FILENAME file has too open permissions"
 else
-        exit 0
+  exit 0
 fi
-
 
 #add environment virable COURSE_ID devsecope12
 COURSE_ID='devsecope12'
 
-
 #Set ZSH as default shell
-if [ ! -z $(which $D_SHELL) ]; then
-  chsh -s $D_SHELL
-fi
+#if [ ! -z $(which $D_SHELL) ]; then
+#  chsh -s $D_SHELL
+#fi
 
 #exit program
 exit 0
