7ac8bf872b628eaba584904d90feade61452913a
yehonatan <yona014@gmail.com>
2023-01-30 23:23:54 +0200

    ex2_third attempt

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
 TEST_PERIODICITY=5
-
 while true; do
-  while read host; do
+  for host in $(cat hosts); do
     timestamp=$(date +%s%N)
-    result=$(ping -c 1 - W 2 $host | grep "1 received")
+    result=$(ping -c 1 -W 2 "$host" | grep "1 received")
     if [ -n "$result" ]; then
       result=1
     else
       result=0
     fi
-    echo "Test result for $host is $result at $timestamp" > curl -i -XPOST "http://localhost:8086/write?db=test_db" \ --data-binary "ping_test value=$resault $timestmp"
+    echo "Test result for $host is $result at $timestamp"
+    curl -i -XPOST "http://localhost:8086/write?db=hosts_metrics" \
+	-u "admin:12345678" \
+  --data-binary "ping_test value=$result $timestamp"
   done < hosts
-  sleep "$TEST_PERIODICITY"
+  sleep $TEST_PERIODICITY
 done
-
-
