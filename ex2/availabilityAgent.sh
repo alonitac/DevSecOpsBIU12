6ee0ba58c51abae3c89019d84c49dfabff5715bd
eli <eli4146@gmail.com>
2023-01-30 10:48:39 +0200

    ex2 first try

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
+export DB_USERNAME=admin
+export DB_PASSWORD=12345678
 
 while true
 do
-  # Your code here....
-
+  while read -r HOST
+  do
+     time=$(date +%s)
+     ping  -c 1 "$HOST"  | awk -v "host=$HOST" -v "time_s=$time" -v "status=0" '/packet loss/ {if ($6 == "0%") status="1" } END { print "Test result for " host  " is " status " at " time_s }'
+     curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$HOST value=1 $time"
+     sleep 1
+  done < hosts
+  echo ""
   sleep "$TEST_PERIODICITY"
 done
