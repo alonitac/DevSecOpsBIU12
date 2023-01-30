fca8aecbcb7e75ee0e68f1c90ae47d723507965f
yehonatan <yona014@gmail.com>
2023-01-30 22:49:34 +0200

    ex2_second attempt

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -11,8 +11,7 @@ while true; do
     else
       result=0
     fi
-    echo "Test result for $host is $result at $timestamp"
-    curl -i -XPOST "http://localhost:8086/write?db=test_db" \ --data-binary "ping_test value=$resault $timestmp"
+    echo "Test result for $host is $result at $timestamp" > curl -i -XPOST "http://localhost:8086/write?db=test_db" \ --data-binary "ping_test value=$resault $timestmp"
   done < hosts
   sleep "$TEST_PERIODICITY"
 done
