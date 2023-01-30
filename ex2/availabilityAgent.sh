dc58f6e1f409d18cf216973f303f277a82b39a09
gal <galdevops@gmail.com>
2023-01-30 09:55:22 +0200

    ex1 by Gal_Y_02 - 1

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,11 @@
-TEST_PERIODICITY=5
-
-while true
+#!/bin/bash
+while IFS="" read -r p || [ -n "$p" ]
 do
-  # Your code here....
+  SEC=$(expr $(ping -c 1 -W 2 "$p" | sed -n 's/.*time=\(.*\) ms$/\1/p'))
+  NAN=$(echo $SEC*1000000 | bc)
+  if [[ $NAN ]]; then printf '%s\n' "Test result for $p is 1 at $NAN"
+  else printf '%s\n' "Test result for $p is 0 at $NAN"
 
-  sleep "$TEST_PERIODICITY"
-done
+
+fi
+done < hosts
