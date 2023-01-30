e7902c9ac451380a58df4903410bfa485980f9d9
yehonatan <yona014@gmail.com>
2023-01-30 22:46:23 +0200

    ex2_first attempt

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
 
-while true
-do
-  # Your code here....
-
+while true; do
+  while read host; do
+    timestamp=$(date +%s%N)
+    result=$(ping -c 1 - W 2 $host | grep "1 received")
+    if [ -n "$result" ]; then
+      result=1
+    else
+      result=0
+    fi
+    echo "Test result for $host is $result at $timestamp"
+    curl -i -XPOST "http://localhost:8086/write?db=test_db" \ --data-binary "ping_test value=$resault $timestmp"
+  done < hosts
   sleep "$TEST_PERIODICITY"
 done
+
+
