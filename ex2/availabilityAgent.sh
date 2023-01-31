a3eef51ae2f4d702c7b7684decb7d54fa1e27f82
aviv singer <aviv.zinger@gmail.com>
2023-01-31 07:49:38 -0800

    first part

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -2,7 +2,13 @@ TEST_PERIODICITY=5
 
 while true
 do
-  # Your code here....
-
+  File="hosts"
+  Lines=$(cat $File)
+   for Line in $Lines
+   do PingOut=$(ping -c 1 -W 2 "$Line")
+   Packet=$(echo "$PingOut" |grep -o -e "1 received" -e "0 received" | grep -o -e "0" -e "1" )
+   Test_Result=$(echo Test result for "$Line" is "$Packet" at "$(date +%s%3N)" )
+   echo "$Test_Result"
+   done
   sleep "$TEST_PERIODICITY"
 done
