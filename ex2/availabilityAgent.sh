88bf1205827f392f3dfd2134d3c19d5701bd72e3
YehonatanLevTovCYBER <yona014@gmail.com>
2023-01-31 13:32:41 +0200

    ex2_attept9

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 TEST_PERIODICITY=5
 export DB_USERNAME=admin
-ecport DB_PASSWORD=12345678
+export DB_PASSWORD=12345678
 #!/bin/bash
 TEST_PERIODICITY=5
 while true; do
-  for TESTED_HOST in $(cat hosts); do
+  while read -r TESTED_HOST; do
     TEST_TIMESTAMP=$(date +%s%N)
     RESULT=$(ping -c 1 -W 2 "$TESTED_HOST" | grep "1 received")
     if [ -n "$RESULT" ]; then
@@ -14,7 +14,7 @@ while true; do
       result=0
     fi
     echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
-    curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+    curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP "
   done < hosts
   sleep $TEST_PERIODICITY
 done
