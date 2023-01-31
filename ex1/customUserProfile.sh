f40fba9da615d1db1b5347a399ff28cc1ab6af2d
ronperez <perez.ron@protonmail.com>
2023-01-31 17:18:53 +0200

    Reduced if condition using double ""

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
-CHECK_UPDATES= /usr/lib/update-notifier/apt-check --human-readable
 FILEPATH="/home/$USER/.token"
 FILENAME=".token"
 D_SHELL="zsh"
 
 #greetings
-echo "Hello" $USER
-
+echo "Hello $USER"
+echo
 #print packages updates
+CHECK_UPDATES="/usr/lib/update-notifier/apt-check --human-readable"
 echo $CHECK_UPDATES
 
 # .token file permission check
-if [[ -f $FILEPATH ]] && [[ $(stat -c "%a" $FILEPATH) != 600 ]]; then
+if [ -f "$FILEPATH" ] && [ "$(stat -c "%a" $FILEPATH)" != "600" ]; then
     echo  "Warning: $FILENAME file has too open permissions"
 fi
 
-#add environment virable COURSE_ID devsecope12
+#add "environment" variable COURSE_ID no export
 COURSE_ID='devsecope12'
 
 #Set ZSH as default shell
