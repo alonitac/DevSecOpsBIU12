a4490ac670a81c7d8e71a1a34210c72be664b99a
Zahi Toozon <toozonzahi@gmail.com>
2023-01-31 11:32:28 -0800

    first

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-TEST_PERIODICITY=5
+TEST_PERIODICITY=3
+RESULT="Test result for"
+
 
 while true
 do
   # Your code here....
+  # Write the result of the host file, Test result for <HOST or IP> is <0 or 1> at <TIME in nanoseconds>
+IFS=''
+while read line
+do
+  echo $RESULT $line
+done < hosts
+echo
 
 
   sleep "$TEST_PERIODICITY"
 done
-
