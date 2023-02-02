a35d13f1cf1d2e32eb5e71de38026766b609ad2f
OmerAvrech <omeravrech@gmail.com>
2023-02-02 13:32:56 +0200

    Finish exercise. Update agent logic include docker-compose for servers.

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,41 @@
+#! /bin/bash
+
 TEST_PERIODICITY=5
 
+# Performing ping request.
+# the function return:
+#     Failed:	0
+#     Success:	RTT (in microseconds)
+function ping_check {
+    if [[ "$1" == "" ]]; then
+        return 0
+    fi
+    result=$(ping $1 -c 1 -W 2 2>/dev/null)
+    if [[ "$?" != "0" ]]; then
+        return 0
+    fi
+    return $(echo "$result" | grep -oP '(?<=time=)\d+(\.\d+)?'| awk '{print $1*1000}')
+}
+
+function execute_check {
+    if [[ "$1" == "" ]]; then
+        return 0
+    fi
+    ping_check "$1"
+    result=$?
+    if [[ result != "0" ]]; then
+        curl -X POST "http://localhost:8086/write?db=hosts_metrics" -u "admin":"12345678" --data-binary "availability_test,host=$1 value=$result $(date +'%s%N')"
+    fi
+}
+
+
+
+hosts=$(cat ./hosts)
+ 
 while true
 do
-  # Your code here....
-
-  sleep "$TEST_PERIODICITY"
+    for host in $hosts; do
+        execute_check "$host" &
+    done
+    sleep "$TEST_PERIODICITY"
 done
