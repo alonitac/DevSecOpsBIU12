2216f7f065d6bca673988b58c2d271757c60c600
omri <omriyx@gmail.com>
2023-02-03 14:36:42 -0800

    update1.2

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -8,22 +8,24 @@ export DB_PASSWORD=12345678
 while true
 do
 # Loop through each line in the file
-while read -r TESTED_HOST; do
+while read -r TESTED_HOST
+ do
 #Check and forward the latency  from Ping command to "RESULT" Variable:
-RESULT=$(ping  -c 1 -W 2 "$TESTED_HOST"  | grep -oP '(?<=time=)\d+(\.\d+)?')
+    RESULT=$(ping  -c 1 -W 2 "$TESTED_HOST"  | grep -oP '(?<=time=)\d+(\.\d+)?')
+      TEST_TIMESTAMP=$(date +%s%N)
 
-# Check if the ping was received and print
-  if [[ $? -ne 0 ]]; then
- #Ping failure:
-    RESULT=0
-  fi
-  TEST_TIMESTAMP=$(date +%s%N)
-  # Report the epoch timestamp in nanoseconds and the test result
-  echo "Ping Result For "Host:" $TESTED_HOST "Latency:"$RESULT "Timestamp:"$TEST_TIMESTAMP"
+# Check if the ping was received and return the value :
+        if [[ -n "$RESULT" ]]
+        then
+          ReturnValue="$RESULT"
+          else
+            ReturnValue=0
+            fi
 
-curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+  echo "Test Result For $TESTED_HOST is $ReturnValue at $TEST_TIMESTAMP"
+  curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$ReturnValue $TEST_TIMESTAMP"
 
-done < hosts
-  echo "<=============================================================>"
+ done < hosts
+  echo ""
   sleep $TEST_PERIODICITY
 done
