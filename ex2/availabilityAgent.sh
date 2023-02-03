585836ad437184a7283d0c690834f71b786e4437
OmriYahav <omriyx@gmail.com>
2023-02-03 18:09:17 +0200

    update1.1

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,29 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
+#To Run :
+export DB_USERNAME=admin
+export DB_PASSWORD=12345678
 
 while true
 do
-  # Your code here....
+# Loop through each line in the file
+while read -r TESTED_HOST; do
+#Check and forward the latency  from Ping command to "RESULT" Variable:
+RESULT=$(ping  -c 1 -W 2 "$TESTED_HOST"  | grep -oP '(?<=time=)\d+(\.\d+)?')
 
-  sleep "$TEST_PERIODICITY"
+# Check if the ping was received and print
+  if [[ $? -ne 0 ]]; then
+ #Ping failure:
+    RESULT=0
+  fi
+  TEST_TIMESTAMP=$(date +%s%N)
+  # Report the epoch timestamp in nanoseconds and the test result
+  echo "Ping Result For "Host:" $TESTED_HOST "Latency:"$RESULT "Timestamp:"$TEST_TIMESTAMP"
+
+curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+
+done < hosts
+  echo "<=============================================================>"
+  sleep $TEST_PERIODICITY
 done
