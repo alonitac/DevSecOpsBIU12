bbe979fe4a612af5f1d90fcdd1970acb483a585a
nalirion <natik.jinji@gmail.com>
2023-02-03 15:15:27 +0200

    my answer

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -2,12 +2,11 @@
 echo "Hello" $USER
 PackagesOutdated= /usr/lib/update-notifier/apt-check --human-readable
 echo $PackagesOutdated
-echo "To see these additional updates run: apt list --upgradable"
 
 
 if [ -f ~/.token ]; then
   if [ $(stat -c "%a" ~/.token) -ne 600 ]; then
-    echo "Warning: .token file has too wide permissions"
+    echo "Warning: .token file has too open permissions"
     else echo "if goes wrong"
   fi
 fi
