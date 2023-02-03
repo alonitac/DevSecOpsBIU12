2a08a4e9885de15b740dd5bf12870cc7f7fd8647
Zahi Toozon <toozonzahi@gmail.com>
2023-02-03 05:05:01 -0800

    Availability Test Agent Without Optional

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-TEST_PERIODICITY=3
-
+TEST_PERIODICITY=5
+export DB_USERNAME=admin
+export DB_PASSWORD=12345678
 
 while true
 do
   # Your code here....
-  # Write the result of the host file, Test result for <HOST or IP> is <0 or 1> at <TIME in nanoseconds>
-IFS=''
 while read line
 do
-  echo "Test result for $line"
+  RESULT=$(ping -c 1 -W 2 "$line"| grep -c "icmp_seq=1")
+  NANOTIME=$(date +%s%N)
+        if [[ $RESULT = 0 ]]
+        then
+          RESULT=0
+          else
+            RESULT=1
+            fi
+        echo Test result for $line is $RESULT at $PINGTIME
+        curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$line value=$RESULT $NANOTIME"
 done < hosts
 echo
-
-
-  sleep "$TEST_PERIODICITY"
+sleep $TEST_PERIODICITY
 done
