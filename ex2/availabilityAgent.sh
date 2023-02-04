c17b52b4ca3a5886ef763dbfce84660bdfcd0f05
eliraz <ovedeliraz96@gmail.com>
2023-02-04 01:29:07 -0800

    bash ex2 wip

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+export DB_USERNAME=admin
+export DB_PASSWORD=12345678
 TEST_PERIODICITY=5
 
-while true
-do
-  # Your code here....
+while true; do
+  while read line; do
+    ping -c 1 -W 2 $line > /dev/null
+    test_result=$?
+    time=$(($(date +%s%N)/1000000))
+    echo "Test result for $line is $test_result at $time"
 
-  sleep "$TEST_PERIODICITY"
+
+curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$line value=$test_result $time"
+  done < hosts.txt
+
+sleep "$TEST_PERIODICITY"
 done
+~
