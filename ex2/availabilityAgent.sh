20d14f43a254b538a634ec166baa021a0352eec6
liorta <lior.taub@icloud.com>
2023-02-04 22:06:52 +0200

    ex2 wip

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
 
 while true
 do
+
   # Your code here....
+  #Read host file and perform ping while printing the result to stdout
+while read line; do
+ ping -c 1 -W 2  "$line"
+done < hosts
 
   sleep "$TEST_PERIODICITY"
 done
