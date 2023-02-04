489afab8d681850cf282a3a61f277771941457ac
liorta <lior.taub@icloud.com>
2023-02-04 19:53:20 +0200

    Final revision ex1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,9 +1,22 @@
-#! /bin/bash
-echo Hello $USER
-echo $(/usr/lib/update-notifier/apt-check --human-readable)
+#!/bin/bash
 
-if [ -f /home/$USER/.token ]; then
-  stat -c "%a" /home/$USER/.token ; then if [ $? != 600 ]; then echo "Warning: .token file has too open permissions
-"
-    
-fi
+# VARIABLES
+TOKEN_PARM=$(stat -c "%a" /home/"$USER"/.token)
+TOKEN_PATH=/home/$USER/.token
+
+# GREETINGS
+echo "Hello $USER"
+
+# PRINT HOW MANY PACKAGES ARE OUTDATED
+/usr/lib/update-notifier/apt-check --human-readable
+
+# CHECK IF .TOKEN EXIST AND ITS PERMISSIONS
+if [[ -f $TOKEN_PATH ]]; then [[ $TOKEN_PARM ]]
+    if [[ $TOKEN_PARM != 600 ]]; then
+      echo "Warning: .token file has too open permissions"
+    fi
+
+# COURSE VARIABLE
+COURSE_ID='devsecops12'fi
+
+# END OF EX1
