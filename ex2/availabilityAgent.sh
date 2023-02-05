c172ded245148d50efd67d2e0fcd818994fa173e
dvir <dvir93ezra@gmail.com>
2023-02-05 22:33:35 +0200

    ex2_05.02.23

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -2,7 +2,32 @@ TEST_PERIODICITY=5
 
 while true
 do
-  # Your code here....
+  while true; do
+    # read each line of the hosts file
+    while read host; do
+     # run the ping command
+    result=$(ping -c 1 -W 2 $host | tail -1)
 
+    # extract the result of the ping
+  if echo $result | grep -q "1 received"; then
+      # ping received, set result to 1
+      result=1
+    else
+      # ping failed, set result to 0
+      result=0
+    fi
+
+  # get the current timestamp in nanoseconds
+  timestamp=$(date +%s%N)
+
+  # write the test result to stdout
+  echo "Test result for $host is $result at $timestamp"
+  done < hosts
+
+  # wait for TEST_PERIODICITY seconds before executing the loop again
   sleep "$TEST_PERIODICITY"
+
 done
+
+done
+
