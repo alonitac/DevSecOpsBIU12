28e1d0c4faf2d8d129b2108721a13642042dc6db
OmerAvrech <omeravrech@gmail.com>
2023-02-05 12:23:18 +0200

    Improve the logic. Using global variable to return values for functions.

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,37 +1,57 @@
 #! /bin/bash
 
+# CONSTANTS
 TEST_PERIODICITY=5
+
+
 
 # Performing ping request.
 # the function return:
 #     Failed:	0
 #     Success:	RTT (in microseconds)
 function ping_check {
+
+    # Check if the input is empty.
+    # If varable is empty the function exit with error code
     if [[ "$1" == "" ]]; then
-        return 0
+        return 1
     fi
-    result=$(ping $1 -c 1 -W 2 2>/dev/null)
+
+    # Performing ping check.
+    # If ping faileed, the function exit with error code
+    __RESULT=$(ping $1 -c 1 -W 2 2>/dev/null)
     if [[ "$?" != "0" ]]; then
-        return 0
+         return 1
     fi
-    return $(echo "$result" | grep -oP '(?<=time=)\d+(\.\d+)?'| awk '{print $1*1000}')
+
+    # Ping working. return the latency in microseconds.
+    echo "$__RESULT" | grep -oP '(?<=time=)\d+(\.\d+)?'| awk '{print $1*1000}'
 }
 
 function execute_check {
+
+    # Check if the input is empty.
+    # If varable is empty the function exit with code 1
     if [[ "$1" == "" ]]; then
-        return 0
+        return 1
     fi
-    ping_check "$1"
-    result=$?
-    if [[ result != "0" ]]; then
-        curl -X POST "http://localhost:8086/write?db=hosts_metrics" -u "admin":"12345678" --data-binary "availability_test,host=$1 value=$result $(date +'%s%N')"
+
+    # Performing ping check for the given address.
+    # Store the result in DB. If DB not avaliable print to screen
+    RESULT=$(ping_check "$1")
+    STATUS="$?"
+    RESULT="${RESULT:=0}"
+    curl -X POST "http://localhost:8086/write?db=hosts_metrics" \
+         -u "admin":"12345678" \
+         --data-binary "availability_test,host=$1 value=$RESULT $(date +'%s%N')" 2> /dev/null
+    POST_RESULT=$?
+    if [[ "$POST_RESULT" != "0" ]]; then
+        echo "Test result for $1 is $STATUS at $(date +'%s%N')"
     fi
 }
 
-
-
 hosts=$(cat ./hosts)
- 
+
 while true
 do
     for host in $hosts; do
