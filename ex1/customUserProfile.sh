88738adb998b43da32fdd75a5ecd8cd893c2bb09
elad <eladtova@gmail.com>
2023-02-06 21:55:05 +0200

    submission 1

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,1 +1,18 @@
-# your solution here...
+#bin/bash
+      #
+      ##greeting the user
+      #echo Hello $user
+      #
+      ##print outdated packages
+      #(/usr/lib/update-notifier/apt-check --human-readble)
+      #
+      ##check .token file permissions
+      #if [ -f ~/.token ]; then
+      #  permissions=$( stat -c "%a" ~/.token)
+      #  if [ "$permissions != "600" ]; then
+      #     echo "warning .token file has too open permissions"
+      #     fi
+      #     fi
+      #
+      ##set course env
+      #course_ID=$devsecops12 solution here...
