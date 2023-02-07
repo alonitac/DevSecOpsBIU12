3a33ffe2fe1a91620841ecc58cc7919a21a77b5e
Guy-BP <guy66bp@gmail.com>
2023-02-07 17:41:50 +0200

    Bash ex2 solution4

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -20,14 +20,19 @@ do
     TEST_TIMESTAMP=$(date +%s)
 
     # Storing the result of the ping command
-    RESULT=$(ping -c 1 -W 2 $HOST)
+    RESULT=$(ping -c 1 -W 2 $HOST | grep "received" | awk '{print $4}')
 
-    # Setting the environment variables for InfluxDB username and password
-    export DB_USERNAME=admin
-    export DB_PASSWORD=12345678
+    # Checking if the result of the ping command is a valid number
+    if [[ $RESULT =~ ^[0-9]+$ ]]; then
+      # Setting the environment variables for InfluxDB username and password
+      export DB_USERNAME=admin
+      export DB_PASSWORD=12345678
 
-    # Writing the result to InfluxDB
-    curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u "$DB_USERNAME:$DB_PASSWORD" --data-binary "availability_test,host=$HOST value=$EXIT_STATUS $TEST_TIMESTAMP"
+      # Writing the result to InfluxDB
+      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u "$DB_USERNAME:$DB_PASSWORD" --data-binary "availability_test,host=$HOST value=$RESULT $TEST_TIMESTAMP"
+    else
+      echo "Invalid result for host $HOST: $RESULT"
+    fi
   done < hosts
 
   # Sleeping for the specified periodicity
