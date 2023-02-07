a77ef9202723e77c203b7abd8de97d802f9e8082
tomy1230 <tomy1230@gmail.com>
2023-02-07 10:21:27 -0800

    ex2_3

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -2,27 +2,25 @@ TEST_PERIODICITY=5
 DB_USERNAME="admin"
 DB_PASSWORD="12345678"
 
-
 while true
 do
- cat hosts | while read TESTED_HOST ;
+  while read  TESTED_HOST
     do
+      TEST_TIMESTAMP=$(date +%s%N)
+      ping -c 1 -W 2 "$TESTED_HOST" > /dev/null
+      if [ $? != 0 ]
+        then
+          RESULT="0"
+        else
+          RESULT="1"
+      fi
 
-        if ping -c 1 -w 2 $TESTED_HOST > /dev/null; then
-       TEST_TIMESTAMP=$( date +%s )
-          echo "Test result for $TESTED_HOST is 1 at $TEST_TIMESTAMP "
-          RESULT=1
-          curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
-          sleep 2
-      else
-         echo "Test result for $TESTED_HOST is 0 at $TEST_TIMESTAMP"
-         RESULT=0
-         curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
-
-        fi
-
-    done
-
-  echo
+      #echo "$TESTED_HOST"
+      echo "Test result for $TESTED_HOST is $RESULT  at $TEST_TIMESTAMP"
+      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+    done < hosts
+    echo
   sleep "$TEST_PERIODICITY"
+
+
 done
