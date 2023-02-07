8332d037c025817c336a5427eefc2973847f0cdc
ofek.harpaz <oharpaz1@Gmail.com>
2023-02-07 04:31:01 -0800

    Test1

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,17 @@
-TEST_PERIODICITY=5
+#!/bin/bash
 
-while true
-do
-  # Your code here....
+TEST_PERIODICITY=5s
 
-  sleep "$TEST_PERIODICITY"
-done
+while true; do
+  read -r line
+  results=$(ping -c 1 -W 2 "$line" | awk -F '/' 'END {print $5}')
+  test_results=$?
+  time=$(date +%s%N)
+  if [ $test_results -eq 0 ]; then
+    test_results=1
+  else
+    test_results=0
+  fi
+  echo "Test result for <$line> is <$test_results> at <$time>"
+  sleep $TEST_PERIODICITY
+done < hosts
