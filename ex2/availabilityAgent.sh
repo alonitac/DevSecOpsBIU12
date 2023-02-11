fa367c55394119c9187fa50353895b4df4bf6dd2
nalirion <natik.jinji@gmail.com>
2023-02-11 22:47:38 +0200

    my answer

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,21 @@
+#!/bin/bash
 TEST_PERIODICITY=5
+export DB_USERNAME="admin"
+export DB_PASSWORD="12345678"
 
 while true
+
 do
-  # Your code here....
+  while read -r TESTED_HOST
+  do
+    RESULT=$( ping -c 1 -W 2 $TESTED_HOST | grep -c "icmp_seq=1" )
+    TEST_TIMESTAMP=$(date +%s)
+      if [ $RESULT -eq 1 ]
+         then echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
+         else echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
+      fi
+
+  done < hosts
 
   sleep "$TEST_PERIODICITY"
 done
