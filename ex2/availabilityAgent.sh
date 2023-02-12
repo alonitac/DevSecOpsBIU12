89b990ced44049e264348982eeb801481acfe7f2
rafael Aviram <Rafael.aviram@gmail.com>
2023-02-12 18:26:37 +0200

    Ex1 completed

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -2,7 +2,19 @@ TEST_PERIODICITY=5
 
 while true
 do
-  # Your code here....
-
+  while read -r SERVER
+  do
+   status_server=$(ping  -c 1 -W 2 "$SERVER"  | grep -oP '(?<=time=)\d+(\.\d+)?')
+   if [[ "$status_server" = "" ]]; then
+     SUCCESS=0
+   else
+     SUCCESS=1
+   fi
+   echo $status_server
+   TIME=$(date +%s)
+   echo "Test result for $SERVER is $SUCCESS at $TIME"
+  done < hosts
   sleep "$TEST_PERIODICITY"
 done
+
+
