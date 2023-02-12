180314fc3a95156b2521eeb5defcd06194a8d8b2
rafael Aviram <Rafael.aviram@gmail.com>
2023-02-12 19:08:56 +0200

    Ex2 completed

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,20 +1,21 @@
 TEST_PERIODICITY=5
-
+export DB_USERNAME=admin
+export DB_PASSWORD=12345678
 while true
 do
-  while read -r SERVER
+  while read -r TESTED_HOST
   do
-   status_server=$(ping  -c 1 -W 2 "$SERVER"  | grep -oP '(?<=time=)\d+(\.\d+)?')
-   if [[ "$status_server" = "" ]]; then
-     SUCCESS=0
-   else
-     SUCCESS=1
-   fi
+   status_server=$(ping  -c 1 -W 2 "$TESTED_HOST"  | grep -oP '(?<=time=)\d+(\.\d+)?')
    echo $status_server
-   TIME=$(date +%s)
-   echo "Test result for $SERVER is $SUCCESS at $TIME"
+   if [[ "$status_server" = "" ]] ; then
+     RESULT=0
+   else
+     RESULT=$status_server
+   fi
+   TEST_TIMESTAMP=$(date +%s)
+   echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
+   curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
   done < hosts
   sleep "$TEST_PERIODICITY"
 done
-
 
