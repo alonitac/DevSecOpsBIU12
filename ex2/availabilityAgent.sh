9e810464795e4344523cb5334c19d319681de119
Sahar <sarjanna@gmail.com>
2023-02-12 11:07:12 +0200

    test

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -10,11 +10,11 @@ else
  echo "test result for $line is 0  at $(date +%s%N)"
 fi
 # echo "test result for <$line> is <$?> at <$(date +%s%N)>"
-gnome-terminal
-ocker run --rm --name influxdb -p 8086:8086 -e INFLUXDB_ADMIN_USER=admin -e INFLUXDB_ADMIN_PASSWORD=12345678 -e INFLUXDB_HTTP_AUTH_ENABLED=true influxdb:1.8.10
-gnome-terminal
+#gnome-terminal
+docker run --rm --name influxdb -p 8086:8086 -e INFLUXDB_ADMIN_USER=admin -e INFLUXDB_ADMIN_PASSWORD=12345678 -e INFLUXDB_HTTP_AUTH_ENABLED=true influxdb:1.8.10
+#gnome-terminal
 curl -X POST 'http://localhost:8086/query' -u admin:12345678 --data-urlencode "q=CREATE DATABASE hosts_metrics"
-gnome-terminal
+#gnome-terminal
 curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
 
 
