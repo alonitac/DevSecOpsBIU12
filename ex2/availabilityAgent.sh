f7088c040f54904c14cccb17bfa430865c86c0ed
ronperez <perez.ron@protonmail.com>
2023-02-13 16:59:19 +0200

    ex2 initial commit. no db, graphana etc...

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -2,7 +2,18 @@ TEST_PERIODICITY=5
 
 while true
 do
-  # Your code here....
-
-  sleep "$TEST_PERIODICITY"
+        # read hosts
+        while read -r host;
+        do
+                p_result=$(ping -c 1 -W 2 "$host")
+                if [ $? -eq 0 ]; then
+                        r_status=1
+                else
+                        r_status=0
+                fi
+                echo "Test result for $host is $r_status at $(date +%s)"
+        done < hosts
+        echo
+        sleep "$TEST_PERIODICITY"
 done
+
