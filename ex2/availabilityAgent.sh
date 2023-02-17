9e6bd0f375123f57d6e11fe18285ebb895069037
nalirion <natik.jinji@gmail.com>
2023-02-17 11:29:03 +0200

    my answer 17.02 11:28 after changed the google to 8.8.8.8

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -13,6 +13,7 @@ do
       if [ $RESULT > "0" ]
          then echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
       fi
+
       curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
   done < hosts
 
