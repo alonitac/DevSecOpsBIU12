e36c42c3e1b6104abfe874937009477d014d4f86
elirambe <eliram26@gmail.com>
2023-02-16 22:54:14 -0800

    test_1

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,38 @@
+#!/bin/bash
+
 TEST_PERIODICITY=5
 
 while true
 do
-  # Your code here....
+  while true; do
+    # read each line of the hosts file
+    while read host; do
+     # run the ping command
+    result=$(ping -c 1 -W 2 $host | tail -1)
 
+    # extract the result of the ping
+  if echo $result | grep -q "1 received"; then
+      # ping received, set result to 1
+      result=1
+    else
+       # ping failed, set result to 0
+      result=0
+    fi
+
+  # get the current timestamp in nanoseconds
+  timestamp=$(date +%s%N)
+
+  # write the test result to stdout
+  echo "Test result for $host is $result at $timestamp"
+  TESTED_HOST=$host
+  RESULT=$result
+  TEST_TIMESTAMP=$timestamp
+  curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+  done < hosts
+
+  # wait for TEST_PERIODICITY seconds before executing the loop again
   sleep "$TEST_PERIODICITY"
+
+done
+
 done
