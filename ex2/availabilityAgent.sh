c79d7ff6a8ba946d48573259d96badfadec683aa
nalirion <natik.jinji@gmail.com>
2023-02-17 16:15:56 +0200

    my answer 17.02 15:48

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,24 @@
+#!/bin/bash
 TEST_PERIODICITY=5
+export DB_USERNAME="admin"
+export DB_PASSWORD="12345678"
 
 while true
+
 do
-  # Your code here....
+  while read -r TESTED_HOST
+  do
+    RESULT=$( ping -c 1 -W 2 $TESTED_HOST | grep -oP '(?<=time=)\d+(\.\d+)?' )
+    TEST_TIMESTAMP=$(date +%s)
+      if [ $RESULT > "0" ]
+         then echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
+         else
+           RESULT=0
+           echo "Test result for $TESTED_HOST is $RESULT at $TEST_TIMESTAMP"
+      fi
+
+      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+  done < hosts
 
   sleep "$TEST_PERIODICITY"
 done
