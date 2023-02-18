e20bcc8d925c2286c477616e5bd54c04411ad4e2
roman <romagaon@gmail.com>
2023-02-18 08:10:03 +0200

    test05

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -3,12 +3,14 @@ DB_USERNAME="admin"
 DB_PASSWORD="12345678"
 TEST_PERIODICITY=5
 TEST_PERIODICITY=5
+
 # While loop read the each line in hosts
 
 while true
 do
 while true; do
   while read host; do
+
 # Run ping command
     if ping -c 1 -W 2 $host > /dev/null; then
       result=1
@@ -19,20 +21,23 @@ while true; do
     # Print the test result to stdout
     echo "Test result for $host is $result at $(date +%s%N)"
 
+
   done < hosts
 
-  # Wait for the next check test ping
+  # Wait for the next ping
   sleep $TEST_PERIODICITY
 
 done
 
-sleep "$TEST_PERIODICITY"
-done
 
 #Write test results to a database
 
-curl -X POST 'http://localhost:8086/query' -u admin:12345678 --data-urlencode "q=CREATE DATABASE hosts_metrics"
-
 curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+done < hosts
+
+# Wait for the next ping
+sleep "$TEST_PERIODICITY"
+
+done
 
 # end
