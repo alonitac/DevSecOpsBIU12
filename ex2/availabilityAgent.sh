7a63c99770d3c74af6a97806288ed8a74851327b
roman <romagaon@gmail.com>
2023-02-18 08:56:56 +0200

    test06

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,43 +1,31 @@
-#!/bin/bash
+TEST_PERIODICITY=5
 DB_USERNAME="admin"
 DB_PASSWORD="12345678"
-TEST_PERIODICITY=5
-TEST_PERIODICITY=5
 
 # While loop read the each line in hosts
-
 while true
 do
-while true; do
-  while read host; do
+  while read  HOSTS
+    do
+      TEST_TIMESTAMP=$(date +%s%N)
+      ping -c 1 -W 2 "$HOSTS" > /dev/null
+      if [ $? != 0 ]
+        then
+          RESULT="0"
+        else
+          RESULT="1"
+      fi
 
-# Run ping command
-    if ping -c 1 -W 2 $host > /dev/null; then
-      result=1
-    else
-      result=0
-    fi
+#InfluxDB instance running
+      curl -X POST 'http://localhost:8086/query' -u admin:12345678 --data-urlencode "q=CREATE DATABASE hosts_metrics"
 
-    # Print the test result to stdout
-    echo "Test result for $host is $result at $(date +%s%N)"
-
-
-  done < hosts
-
-  # Wait for the next ping
-  sleep $TEST_PERIODICITY
-
-done
-
-
-#Write test results to a database
-
-curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
-done < hosts
-
+# Print the test result to stdout
+      echo "Test result for $HOSTS is $RESULT  at $TEST_TIMESTAMP"
+      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' -u $DB_USERNAME:$DB_PASSWORD  --data-binary "availability_test,host=$HOSTS value=$RESULT $TEST_TIMESTAMP"
+    done < hosts
+    echo
 # Wait for the next ping
-sleep "$TEST_PERIODICITY"
+  sleep "$TEST_PERIODICITY"
+
 
 done
-
-# end
