394184da7677214d5af17fa7bccedd41cd70b3cb
shlomi <shlomi57000@gmail.com>
2023-02-18 07:41:32 -0800

    bash ex1 solution

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,13 +1,12 @@
-# your solution here...
 #!bin/bash
-sudo apt-get update
+echo "Hello $USER"
 
-echo "Hello" $USER
-/usr/lib/update-notifier/apt-check --human-readable
+echo "Update available: $(/usr/lib/update-notifier/apt-check --human-readable)"
 
-if [[ -f ~/.token ]] ; then
-if [[ "$(stat -c %a ~/.token)" -ne 600 ]]; then
-   echo "Warning: .token file has too open permissions"
- fi
- COURSE_ID="devsecops12"
+if [[ -e "/home/$USER/.token" ]]; then
+    permissions=$(stat -c "%a" "/home/$USER/.token")
+    if [[ "$permissions" != "600" ]]; then
+        echo "Warning: .token file has too open permissions"
+    fi
+COURSE_ID="devsecops12"
 fi
