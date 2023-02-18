3568fb845bd387cb704765a71acc6f3bcf97097c
shlomi <shlomi57000@gmail.com>
2023-02-18 07:31:00 -0800

    bash ex1 solution

--- ex1/customUserProfile.sh
+++ ex1/customUserProfile.sh
@@ -1,25 +1,11 @@
 # your solution here...
 #!bin/bash
-
-echo "Hello $USER"
-echo -e '\n'
+echo "Hello" $USER
 /usr/lib/update-notifier/apt-check --human-readable
-echo -e '\n'
 
-t=$HOME/.token
-
-# check if the .token file exists in the home directory
-if [ -f "$t" ]; then
-  # get the octal representation of the file permission
-  perm=$(stat -c "%a" "$t")
-
-  # check if the permission is different from 600
-  if [ "${perm}" != "600" ]; then
-    echo "Warning: .token file has too open permissions"
-  fi
-else
-  echo "Error: .token file not found in home directory"
+if [[ -f ~/.token ]] ; then
+if [[ "$(stat -c %a ~/.token)" -ne 600 ]]; then
+   echo "Warning: .token file has too open permissions"
+ fi
+ COURSE_ID="devsecops12"
 fi
-
-
-export COURSE_ID=devsecops12
