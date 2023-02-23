ae119faf13d75b8c85509b2a5773d8093ef92fed
Eli Levy <eli4146@gmail.com>
2023-02-23 18:25:52 +0200

    ex4 second test

--- ex4_optional/tlsHandshake.sh
+++ ex4_optional/tlsHandshake.sh
@@ -9,7 +9,7 @@ echo $serverCert >> cert.pem
 
 #echo "serverVersion = $serverVersion    SESSION_ID=$SESSION_ID="
 
-# wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+wget https://devops-may22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
 
 VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
 
