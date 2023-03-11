8530563c2c6ba67de4783ed9ecb7b6dbfc6f4d46
Yuval <yuvalarviv1122@icloud.com>
2023-03-11 07:19:33 +0200

    ex2

--- ex2/availabilityAgent.sh
+++ ex2/availabilityAgent.sh
@@ -1,8 +1,39 @@
 TEST_PERIODICITY=5
 
-while true
-do
-  # Your code here....
+#!/bin/bash
 
-  sleep "$TEST_PERIODICITY"
+# Define constants
+HOSTS_FILE="hosts"
+TEST_PERIODICITY=5
+DB_USERNAME="admin"
+DB_PASSWORD="12345678"
+
+# Iterate forever
+while true; do
+
+  # Iterate over the lines of the hosts file
+  while read line; do
+    # Trim leading/trailing whitespace
+    line="$(echo -e "${line}" | tr -d '[:space:]')"
+    if [ ! -z "${line}" ]; then
+      # Ping the host and get the result
+      if ping -c 1 -W 2 "${line}" > /dev/null 2>&1; then
+        result=1
+      else
+        result=0
+      fi
+
+      # Print the test result to stdout
+      echo "Test result for ${line} is ${result} at $(date +%s%N)"
+
+      # Write the test result to InfluxDB
+      TESTED_HOST="$line" RESULT="$result" TEST_TIMESTAMP="$(date +%s%N)" \
+      curl -X POST 'http://localhost:8086/write?db=hosts_metrics' \
+        -u $DB_USERNAME:$DB_PASSWORD \
+        --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+    fi
+  done < "${HOSTS_FILE}"
+
+  # Wait for the next iteration
+  sleep ${TEST_PERIODICITY}
 done
